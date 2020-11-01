4c82c4e78548281e6259480d64b1d5a1f16cb9cb
Xenon <s10110040@gmail.com>
2020-11-02 07:13:46 +0800

    image encryption and decryption done

--- src/main/java/com/ssss/CD1/ImageResources.java
+++ src/main/java/com/ssss/CD1/ImageResources.java
@@ -8,6 +8,7 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -26,29 +27,27 @@ import ssss.MakeSharePlus;
 import ssss.RecoverSecretPlus;
 
 @Path("ImageApi")
+@PermitAll
 public class ImageResources {
 
 	@POST
-	@Path("/encryption")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("/encryption/{n}/{t}")
+	@Consumes({ MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_JSON })
-	public Response getShares( @FormDataParam("image") InputStream fileInputStream) {
-		System.out.println("You are now in Image encryption service");
-//		System.out.println("map content: " + map);
-		
+	public Response getShares(@FormDataParam("image") InputStream fileInputStream,@PathParam("t") int t,@PathParam("n") int n) {
+
 		try {
-			int n=3;
-			int t=2;
+			System.out.println("You are now in Image encryption service");
 			byte[] temp = IOUtils.toByteArray(fileInputStream);
-			
-			byte []secretByte=Arrays.copyOfRange(temp,0,temp.length-1);
+
+			byte[] secretByte = Arrays.copyOfRange(temp, 0, temp.length - 1);
 //			System.out.println("secret: "+Arrays.toString(secretByte));
 			MakeSharePlus makeSharePlus = new MakeSharePlus(secretByte, t, n, 8);
-			
+
 			String[] shares = makeSharePlus.constructPoints();
 			System.out.println("points(shares): " + Arrays.deepToString(shares) + "\n");
 
-			//I still cannot open the image even I　do this>.<
+			// I still cannot open the image even I do this>.<
 //			int numOfByte = shares[0].split("-").length;
 //			byte[][] secretEncrypt = new byte[n][numOfByte - 1];// 1st is not share
 //			String[][] sharePerByte = new String[n][numOfByte];
@@ -62,29 +61,27 @@ public class ImageResources {
 //					secretEncrypt[i][k-1] = (byte)(Integer.parseInt(sharePerByte[i][k]));
 //				}
 //			}
-				
-			
-			Map<String, Object> result = new HashMap<String,Object>();
-			for(int i=0;i<n;i++)
-			{
+
+			Map<String, Object> result = new HashMap<String, Object>();
+			for (int i = 0; i < n; i++) {
 //				result.put("share"+i,Base64.getEncoder().encodeToString(secretEncrypt[i]) );
-				result.put("share"+i,shares[i] );
+				result.put("share" + i, shares[i]);
 			}
 			System.out.print(result);
 			Response reply = Response.ok(result).build();
 			return reply;
 
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
-		
 
 		return null;
 	}
 
 	@POST
 	@Path("/recovery/{t}")
+	@Consumes({ MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_JSON })
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getSecret(@PathParam("t") int t, Map<String, Object> map) {
 		System.out.println("You are now in text recovery service");
