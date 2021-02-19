c07c9b3f7b5046709edee3d15193ae9babb785ba
Xenon <s10110040@gmail.com>
2021-02-19 11:14:18 +0800

    fix unit test bugs

--- src/test/java/ImageResource/ImageResourceTest.java
+++ src/test/java/ImageResource/ImageResourceTest.java
@@ -1,8 +1,7 @@
 package ImageResource;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
@@ -10,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ProtocolException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -20,7 +20,7 @@ import javax.ws.rs.core.StreamingOutput;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+
 
 import com.ssss.CD1.ImageResources;
 
@@ -74,19 +74,38 @@ public class ImageResourceTest {
 	//test the sucessful upload
 	@Test
 	public void checkresutnresponsesize_recovery() throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("src/test/java/ImageResource/image.png");
-		byte[] secretByte = IOUtils.toByteArray(fileInputStream);
+//		FileInputStream fileInputStream = new FileInputStream("src/test/java/ImageResource/image.png");
+//		byte[] secretByte = IOUtils.toByteArray(fileInputStream);
 		ImageResources imageresource = new ImageResources();
-		Response testresponse_encryption = null;
-		testresponse_encryption = imageresource.getShares(fileInputStream, 2, 2);
-		Map<String, Object> messageEntity = (Map<String, Object>) testresponse_encryption.getEntity();
+//		Response testresponse_encryption = null;
+//		testresponse_encryption = imageresource.getShares(fileInputStream, 2, 2);
+//		Map<String, Object> messageEntity = (Map<String, Object>) testresponse_encryption.getEntity();
+//		
+//		Response testresponse_recovery = null;
+//		testresponse_recovery = imageresource.getSecret(2, messageEntity);
+//		Map<String, Object> returnresponse = (Map<String, Object>) testresponse_recovery.getEntity();
+//		System.out.println("the size of original file: " + secretByte.toString().length());
+//		System.out.println("the return size: " + returnresponse.get("secret").toString().length());
+//		assertEquals(secretByte.toString().length(),returnresponse.get("secret").toString().length());
+		
+		
+		Map<String, Object > map=  new HashMap<String, Object>();;
+	//	share0=AlbLQOKF6w==, share1=AY/Dh19t1Q==}
+		
+		
+		//IF the recovery process is correct, the below shares will be recovered as "123456", we just want to test recovery here
+		map.put("share0","iVBORw0KGgoAAAANSUhEUgAAAAcAAAABCAIAAACdaSOZAAAAHklEQVR4XmNgYGD8//8HAwMHA4M9A4MMA4MoA0M+ADbrA9+g9pbzAAAAAElFTkSuQmCC");
+		map.put("share1","iVBORw0KGgoAAAANSUhEUgAAAAcAAAABCAIAAACdaSOZAAAAHklEQVR4XmNgYGD6/38HA4MbA4M2A0MKA0Pp//8tAD3kBoXshw5RAAAAAElFTkSuQmCC");
 		
 		Response testresponse_recovery = null;
-		testresponse_recovery = imageresource.getSecret(2, messageEntity);
-		Map<String, Object> returnresponse = (Map<String, Object>) testresponse_recovery.getEntity();
-		System.out.println("the size of original file: " + secretByte.toString().length());
-		System.out.println("the return size: " + returnresponse.get("secret").toString().length());
-		assertEquals(secretByte.toString().length(),returnresponse.get("secret").toString().length());
+		testresponse_recovery = imageresource.getSecret(2, map);
+		String recoveredSecret=new String(((Map<String, byte[]>)testresponse_recovery.getEntity()).get("secret"),StandardCharsets.UTF_8);
+		System.out.println("The recovered Image Secret "+ recoveredSecret);
+		
+//		Map<String, Object> returnresponse = (Map<String, Object>) testresponse_recovery.getEntity();
+//		System.out.println("the size of original file: " + secretByte.toString().length());
+//		System.out.println("the return size: " + returnresponse.get("secret").toString().length());
+		assertEquals(recoveredSecret,"123456");
 		
 	}
 	
--- src/test/java/ZipResourcesTest/ZipResourcesTest.java
+++ src/test/java/ZipResourcesTest/ZipResourcesTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -63,19 +65,29 @@ public class ZipResourcesTest {
 	//test the sucessful upload
 	@Test
 	public void checkresutnresponsesize_recovery() throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("src/test/java/ZipResourcesTest/test_file.zip");
-		byte[] secretByte = IOUtils.toByteArray(fileInputStream);
+//		FileInputStream fileInputStream = new FileInputStream("src/test/java/ZipResourcesTest/test_file.zip");
+//		byte[] secretByte = IOUtils.toByteArray(fileInputStream);
 		ZipResources zipresource = new ZipResources();
-		Response testresponse_encryption = null;
-		testresponse_encryption = zipresource.getShares(fileInputStream, 2, 2);
-		Map<String, Object> messageEntity = (Map<String, Object>) testresponse_encryption.getEntity();
+//		Response testresponse_encryption = null;
+//		testresponse_encryption = zipresource.getShares(fileInputStream, 2, 2);
+//		Map<String, Object> messageEntity = (Map<String, Object>) testresponse_encryption.getEntity();
+		Map<String, Object > map=  new HashMap<String, Object>();;
+	//	share0=AlbLQOKF6w==, share1=AY/Dh19t1Q==}
+		
+		
+		//IF the recovery process is correct, the below shares will be recovered as "123456", we just want to test recovery here
+		map.put("share0","AlbLQOKF6w==");
+		map.put("share1","AY/Dh19t1Q==");
 		
 		Response testresponse_recovery = null;
-		testresponse_recovery = zipresource.getSecret(2, messageEntity);
-		Map<String, Object> returnresponse = (Map<String, Object>) testresponse_recovery.getEntity();
-		System.out.println("the size of original file: " + secretByte.toString().length());
-		System.out.println("the return size: " + returnresponse.get("secret").toString().length());
-		assertEquals(secretByte.toString().length(),returnresponse.get("secret").toString().length());
+		testresponse_recovery = zipresource.getSecret(2, map);
+		String recoveredSecret=new String(((Map<String, byte[]>)testresponse_recovery.getEntity()).get("secret"),StandardCharsets.UTF_8);
+		System.out.println("The recovered Zip Secret "+ recoveredSecret);
+		
+//		Map<String, Object> returnresponse = (Map<String, Object>) testresponse_recovery.getEntity();
+//		System.out.println("the size of original file: " + secretByte.toString().length());
+//		System.out.println("the return size: " + returnresponse.get("secret").toString().length());
+		assertEquals(recoveredSecret,"123456");
 		
 	}
 	
